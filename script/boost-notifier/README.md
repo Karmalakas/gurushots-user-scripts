29721a11b069b2f1d55adafc79c2dc435125320e
Karmalakas <cronus.raven@gmail.com>
2025-01-03 16:18:55 +0200

    Add Boost Notifier script

---
+++ script/boost-notifier/README.md
@@ -0,0 +1,9 @@
+This script might help you notice when the boost is available for challenge.
+What this script does, is just find all active Boost buttons and add a green(ish) border around the challenge block
+
+> [!INFO]  
+> Old script with comments can be found on [this gist](https://gist.github.com/Karmalakas/dd3e9d1744a67dd6fd2272c652bbea66)
+
+---
+
+![boost-border.png](images/boost-border.png)
---
+++ script/boost-notifier/script.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         GuruShots boost notifier
+// @description  Mark challenge when boost is available
+// @namespace    http://karmalakas.lt/
+// @version      1.2.0
+// @author       Karmalakas
+// @updateURL    https://github.com/Karmalakas/gurushots-user-scripts/blob/main/script/boost-notifier/script.user.js
+// @downloadURL  https://github.com/Karmalakas/gurushots-user-scripts/blob/main/script/boost-notifier/script.user.js
+// @supportURL   https://github.com/Karmalakas/gurushots-user-scripts/issues
+// @match        https://gurushots.com/*
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var mutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+
+    function process() {
+        var buttons = document.querySelectorAll(
+            'app-active-challenge-action-button-component .blink[class^="boost"],app-active-challenge-action-button-component .blink[class*=" boost"]'
+        );
+
+        if (!buttons.length) {
+            return;
+        }
+
+        for (var i = 0; i < buttons.length; i++) {
+            processButton(buttons[i]);
+        }
+    }
+
+    function processButton(button) {
+        var challengeHolder = button.closest('challenges-item').style['boxShadow'] = '0 0 0 7px rgba(151,255,151,0.75)';
+    }
+
+    if (mutationObserver) {
+        var body = document.querySelector('body');
+        if (!body) {
+            return;
+        }
+
+        (new mutationObserver(process)).observe(body, {
+            'childList': true,
+            'subtree': true
+        });
+    }
+})();
