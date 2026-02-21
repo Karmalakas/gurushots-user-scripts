b7b8d103183e431914451d2e7203d4702c210c39
Karmalakas <cronus.raven@gmail.com>
2026-02-21 21:47:11 +0200

    Add points difference value during the match

--- script/team-points/script.user.js
+++ script/team-points/script.user.js
@@ -2,7 +2,7 @@
 // @name         GuruShots team points
 // @description  Show team points based on current vote count in a team battle
 // @namespace    http://karmalakas.lt/
-// @version      1.1.1
+// @version      1.2.0
 // @author       Karmalakas
 // @updateURL    https://github.com/Karmalakas/gurushots-user-scripts/raw/refs/heads/main/script/team-points/script.user.js
 // @downloadURL  https://github.com/Karmalakas/gurushots-user-scripts/raw/refs/heads/main/script/team-points/script.user.js
@@ -16,6 +16,23 @@
     'use strict';
 
     GM_addStyle('' +
+        '.match-active-header > app-ng-image-component.match-active-header__vs {' +
+        '    display: block;' +
+        '    text-align: center;' +
+        '}' +
+        '.match-active-header > app-ng-image-component.match-active-header__vs div[class^="diff-"] {' +
+        '    font-weight: 700;' +
+        '    font-size: 16px;' +
+        '    margin-top: -22px;' +
+        '}' +
+        '.match-active-header > app-ng-image-component.match-active-header__vs div.diff-red {' +
+        '    color: #933;' +
+        '    text-shadow: 0px 0px 3px #F00;' +
+        '}' +
+        '.match-active-header > app-ng-image-component.match-active-header__vs div.diff-green {' +
+        '    color: #070;' +
+        '    text-shadow: 0px 0px 3px #070;' +
+        '}' +
         '.match-active-header__item-content__badge-wrapper__total-votes {' +
         '    padding: 0 8px;' +
         '    width: auto;' +
@@ -56,6 +73,11 @@
         }
 
         processData(data);
+        addDiff(
+            matchHeader.querySelector(':scope > app-ng-image-component.match-active-header__vs'),
+            data[0].votes,
+            data[1].votes
+        );
     }
 
     function processData(data) {
@@ -79,6 +101,20 @@
         el.innerText = result;
 
         holder.appendChild(el);
+    }
+
+    function addDiff(holder, home, opponent) {
+        const diff = home - opponent;
+
+        if (diff === 0) {
+            return;
+        }
+
+        const el = document.createElement('div');
+        el.className = diff < 0 ? 'diff-red' : 'diff-green';
+        el.innerText = diff.toString();
+
+        holder.lastChild.replaceWith(el);
     }
 
     mutationObserver && new MutationObserver(function (mut, observer) {
